a60a497c309dc1c5ba2f097702504270d0ded3aa
ChengHsin Lin <chenghsinlin@ChengHsins-MacBook-Pro.local>
2025-11-30 19:40:49 +0800

    feat(seuol-app): v1 seuol app

--- seoul-trip-2025/vite.config.js
+++ seoul-trip-2025/vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // ⚠️ 重要：請將 'seoul-trip-2025' 改成您在 GitHub 上面建立的專案名稱 (Repository Name)
   // 如果您的網址是 https://xxx.github.io/my-trip/，這裡就是 '/my-trip/'
-  base: '/vibe_coding-useful_tools/seuol/', 
+  base: '/vibe_coding-useful_tools/', 
 })
